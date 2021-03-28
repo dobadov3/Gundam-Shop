f7b2b63e045bedddfd2aa9d0e793202628366a60
dobadov3 <dobadov3@gmail.com>
2021-03-28 16:00:26 +0700

    Cart + Wishlist Page

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -4,6 +4,10 @@ var Product = require('../models/products.model');
 module.exports.get = async function(req, res){
     var products = await Product.find();
 
+    products.forEach(product => {
+        product.priceSale = product.price - (product.price*product.sale)/100
+    });
+
     res.render('./home/index', {
         data: data.data,
         products: products.slice(0, 4)
